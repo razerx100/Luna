fc2d3e0f0c904b9effeec86cd45b18623116bfa9
razerx100 <saikatmandal8@gmail.com>
2021-12-18 00:40:47 +0600

    updated GraphicEngine interface

--- templates/IGraphicsEngine.hpp
+++ templates/IGraphicsEngine.hpp
@@ -11,23 +11,19 @@ public:
 	virtual ~GraphicsEngine() = default;
 
 	virtual void SetBackgroundColor(const Ceres::VectorF32& colorVector) noexcept = 0;
-	virtual void SubmitModels(const class IModel* const models, std::uint32_t modelCount) = 0;
+	virtual void SubmitModels(
+		class IModel* models, std::uint32_t modelCount,
+		bool texture = true
+	) = 0;
 	virtual void Render() = 0;
 	virtual void Resize(std::uint32_t width, std::uint32_t height) = 0;
 	virtual void GetMonitorCoordinates(
 		std::uint64_t& monitorWidth, uint64_t& monitorHeight
 	) = 0;
 	virtual void WaitForAsyncTasks() = 0;
-};
 
-__declspec(dllimport) GraphicsEngine* __cdecl GetGraphicsEngineInstance() noexcept;
-__declspec(dllimport) void __cdecl InitGraphicsEngineInstance(
-	const char* appName,
-	void* windowHandle,
-	void* moduleHandle,
-	std::uint32_t width, std::uint32_t height,
-	std::uint8_t bufferCount = 2u
-);
-__declspec(dllimport) void __cdecl CleanUpGraphicsEngineInstance() noexcept;
+	virtual void SetShaderPath(const char* path) noexcept = 0;
+	virtual void InitResourceBasedObjects() = 0;
+};
 
 #endif
